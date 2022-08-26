1ecae6335d8bbaf9b605e226dd960d00aa15022a
TinkBeth <104775203+TinkBeth@users.noreply.github.com>
2022-08-26 10:40:26 -0300

    Version1
    primera prueba autonoma

---
+++ archivo-ejemplo.js
@@ -0,0 +1,6 @@
+primer ejemplo desde vsc
+agrego a mi repo local con "git add <ejemplo.js>"
+guardo en repo local y dejo msj con "git commit <ejemplo.js"
+reviso si hay cambios guardados en local con "git status" 
+envio cambios a repo remoto con "git push"
+ 
